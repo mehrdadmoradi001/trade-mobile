b071a5f64abfe4bf55a01c406808073bf3dab9b8
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-08-15 23:08:37 +0430

    changes color Exit

--- lib/statistics_page.dart
+++ lib/statistics_page.dart
@@ -6,8 +6,6 @@ import 'package:trade_mobile_flutter/login_widget/login_page.dart';
 class StatisticsPage extends StatelessWidget {
   const StatisticsPage({Key? key}) : super(key: key);
 
-
-
   void getNavigator(BuildContext context, Widget page) {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -33,8 +31,8 @@ class StatisticsPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              CustomStatisticsPage('a', 'برای 25 آبان Alchemy pay : سیگنال خرید',
-                  '12,365', '12,560'),
+              CustomStatisticsPage('a',
+                  'برای 25 آبان Alchemy pay : سیگنال خرید', '12,365', '12,560'),
               SizedBox(height: 25),
               CustomStatisticsPage(
                   'c', 'برای 25 آبان Cosmos : سیگنال خرید', '12,365', '12,560'),
@@ -42,19 +40,20 @@ class StatisticsPage extends StatelessWidget {
               CustomStatisticsPage(
                   'r', 'برای 25 آبان Ripple : سیگنال خرید', '12,365', '12,560'),
               SizedBox(height: 25),
-              CustomStatisticsPage(
-                  's', 'برای 25 آبان SafeMoon : سیگنال خرید', '12,365', '12,560'),
+              CustomStatisticsPage('s', 'برای 25 آبان SafeMoon : سیگنال خرید',
+                  '12,365', '12,560'),
               SizedBox(height: 30),
               Padding(
                 padding: EdgeInsets.only(bottom: 16),
                 child: TextButton(
                   onPressed: () {
-                    getNavigator(context , LoginPage());
+                    getNavigator(context, LoginPage());
                   },
                   child: CenterText(
                       text: 'خروج از حساب',
                       fontWeight: FontWeight.bold,
-                      fontSize: 18),
+                      fontSize: 18,
+                      color: Colors.red),
                 ),
               ),
             ],
